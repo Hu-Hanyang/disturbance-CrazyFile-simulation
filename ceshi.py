b5b47e03962ba137ded7bb94256c87ff17abaa4f
hha160 <HuHY97@outlook.com>
2024-04-09 10:56:41 -0700

    start to separate the alg and the env

--- ceshi.py
+++ ceshi.py
@@ -226,6 +226,7 @@ def create_gif(image_list, filename, duration=0.1):
 
 # env = gym.make('DroneHoverBulletEnvWithRandomHJAdversary-v0')
 env = DroneHoverBulletEnvWithAdversary(observation_noise=-1)
+print(f"The observation space is {env.observation_space}.")
 
 ckpt = "results_train_crazyflie/DroneHoverBulletEnvWithRandomHJAdversary-v0/ppo/2024_03_18_21_33/seed_40226"
 # action = env.action_space.sample()
@@ -236,54 +237,56 @@ print(f"The shape of the observation space is {env.observation_space.shape}")
 
 print(f"The observation noise is {env.observation_noise}")
 
-# init_obs = env.reset()
-# print(f"The initila pos is {init_obs[0:3]}")
-# env.step(action)
+# # init_obs = env.reset()
+# # print(f"The initila pos is {init_obs[0:3]}")
+# # env.step(action)
 
-# check performances
-print(f"The disturbance level is {env.disturbance_level}")
-num_gifs = 1
-frames = [[] for _ in range(num_gifs)]
+# # check performances
+# print(f"The disturbance level is {env.disturbance_level}")
+# num_gifs = 1
+# frames = [[] for _ in range(num_gifs)]
 
-num=0
-while num < num_gifs:
-    terminated, truncated = False, False
-    rewards = 0.0
-    steps = 0
-    max_steps=50
-    init_obs = env.reset()
-    print(f"The init_obs shape is {init_obs.shape}")
-    print(f"The initial position is {init_obs[0:3]}")
-    frames[num].append(env.capture_image())  # the return frame is np.reshape(rgb, (h, w, 4))
+# num=0
+# while num < num_gifs:
+#     terminated, truncated = False, False
+#     rewards = 0.0
+#     steps = 0
+#     max_steps=50
+#     init_obs = env.reset()
+#     print(f"The init_obs shape is {init_obs.shape}")
+#     print(f"The initial position is {init_obs[0:3]}")
+#     frames[num].append(env.capture_image())  # the return frame is np.reshape(rgb, (h, w, 4))
     
-    for _ in range(max_steps):
-        if _ == 0:
-            obs = init_obs
+#     for _ in range(max_steps):
+#         if _ == 0:
+#             obs = init_obs
 
-        # Select control
-        # manual control
-        motor = 0.0
-        # action = np.array([motor, motor, motor, motor])
-        # random control
-        # action = env.action_space.sample()
-        # load the trained model
-        ac, trained_env, env_distb = utils.load_actor_critic_and_env_from_disk(ckpt)
-        ac.eval()
-        obs = torch.as_tensor(obs, dtype=torch.float32)
-        action, *_ = ac(obs)
-
-        obs, reward, done, info = env.step(action)
-        print(f"The shape of the obs in the output of the env.step is {obs.shape}")
-        # print(f"The current reward of the step{_} is {reward} and this leads to {terminated} and {truncated}")
-        # print(f"The current penalty of the step{_} is {info['current_penalty']} and the current distance is {info['current_dist']}")
-        frames[num].append(env.capture_image())
-        rewards += reward
-        steps += 1
+#         # Select control
+#         # manual control
+#         motor = 0.0
+#         action = np.array([motor, motor, motor, motor])
         
-        if done or steps>=max_steps:
-            print(f"[INFO] Test {num} is done with rewards = {rewards} and {steps} steps.")
-            create_gif(frames[num], f'check_env_gif{num}-motor{motor}-{steps}.gif', duration=0.1)
-            print(f"The final position is {obs[0:3]}.")
-            num += 1
-            break
-env.close()
+#         # random control
+#         # action = env.action_space.sample()
+
+#         # # load the trained model
+#         # ac, trained_env, env_distb = utils.load_actor_critic_and_env_from_disk(ckpt)
+#         # ac.eval()
+#         # obs = torch.as_tensor(obs, dtype=torch.float32)
+#         # action, *_ = ac(obs)
+
+#         obs, reward, done, info = env.step(action)
+#         # print(f"The shape of the obs in the output of the env.step is {obs.shape}")
+#         # print(f"The current reward of the step{_} is {reward} and this leads to {terminated} and {truncated}")
+#         # print(f"The current penalty of the step{_} is {info['current_penalty']} and the current distance is {info['current_dist']}")
+#         frames[num].append(env.capture_image())
+#         rewards += reward
+#         steps += 1
+        
+#         if done or steps>=max_steps:
+#             print(f"[INFO] Test {num} is done with rewards = {rewards} and {steps} steps.")
+#             create_gif(frames[num], f'check_env_gif{num}-motor{motor}-{steps}.gif', duration=0.1)
+#             # print(f"The final position is {obs[0:3]}.")
+#             num += 1
+#             break
+# env.close()
